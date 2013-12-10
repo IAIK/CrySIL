d05d5688eb2a709e50652f3fa8a135d65273493c
Otto Touzil <otto.touzil@gmail.com>
2013-12-10 15:16:15 +0100

    jvm.h added :)

---
+++ C/src/jvm.h
@@ -0,0 +1,74 @@
+#include <jni.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef __JVM_H
+#define __JVM_H
+
+#define JAVACLASSNAME "pkcs11Interface"
+
+
+
+//struct jvm_singleton;
+
+
+typedef struct jvm_singleton
+{
+	JavaVMOption options[1];
+	JNIEnv *env;
+	JavaVM *jvm;
+	JavaVMInitArgs vm_args;
+	long status;
+	jclass cls;
+	jmethodID mid;
+	jint square;
+	//jboolean not;
+}sing;
+
+struct jvm_singleton* get_instance()
+{
+    static struct jvm_singleton* instance = NULL;
+
+    if (instance == NULL)
+    {
+
+	instance =(jvm_singleton*) malloc(sizeof(jvm_singleton));
+	if(instance==NULL){
+		//out of memory --> die gracefully
+	}
+	instance->options[0].optionString = "-Djava.class.path=.";
+	memset(&(instance->vm_args), 0, sizeof(instance->vm_args));
+	instance->vm_args.version = JNI_VERSION_1_2;
+	instance->vm_args.nOptions = 1;
+	instance->vm_args.options = instance->options;
+	instance->status = JNI_CreateJavaVM(&instance->jvm, (void**)&(instance->env), &(instance->vm_args));
+	if(instance->status==JNI_ERR){
+		//VM - Error --> cleanup & die gracefully 
+	}
+	instance->cls = ((instance->env))->FindClass(JAVACLASSNAME);
+	if(instance->cls ==0){
+		printf("Class not found!");
+
+	}
+
+
+    }
+
+    return instance;
+}
+
+void destroyVM(){
+struct jvm_singleton* dings = get_instance();
+//(dings->jvm)->DestroyJavaVM(jvm);
+
+}
+
+#ifdef _WIN32
+#define PATH_SEPARATOR ';'
+#else
+#define PATH_SEPARATOR ':'
+#endif
+
+
+
+#endif
