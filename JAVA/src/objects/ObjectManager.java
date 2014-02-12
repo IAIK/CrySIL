c89c189cea354c7a7bb395121f4741131c366248
Otto Touzil <otto.touzil@gmail.com>
2014-02-12 17:53:44 +0100

    objectmanager integrated...

--- JAVA/src/objects/ObjectManager.java
+++ JAVA/src/objects/ObjectManager.java
@@ -2,6 +2,7 @@ package objects;
 
 import java.util.ArrayList;
 
+import pkcs11.PKCS11Error;
 import proxys.CK_ATTRIBUTE;
 
 public class ObjectManager {
@@ -9,28 +10,37 @@ public class ObjectManager {
 	private ArrayList<PKCS11Object> objects = new ArrayList<>();
 	private ArrayList<Long> ids = new ArrayList<>();
 
-	synchronized public ArrayList<PKCS11Object> getObjects(
-			CK_ATTRIBUTE[] template) {
+	synchronized public ArrayList<PKCS11Object> getObjects(CK_ATTRIBUTE[] template) throws PKCS11Error {
 		PKCS11Object object = ObjectBuilder.createFromTemplate(template);
 		ArrayList<PKCS11Object> result = new ArrayList<>();
 
 		for (PKCS11Object tmp : objects) {
-			if (tmp.matches(object)) {
+			if (tmp.equals(object)) {
 				result.add(tmp);
 			}
 		}
 		return result;
 	}
 
-	synchronized public long createObject(CK_ATTRIBUTE[] template) {
+	synchronized public long createObject(CK_ATTRIBUTE[] template) throws PKCS11Error {
 		Long id = getNextId();
 		PKCS11Object object = ObjectBuilder.createFromTemplate(template);
 		objects.add(object);
 		ids.add(id);
 		return id;
 	}
-
-
+	
+	synchronized public void deleteObject(long id){
+		for(Long tmp: ids){
+			if(tmp == id){
+				int index = ids.indexOf(tmp);
+				ids.remove(index);
+				objects.remove(index);
+				return;
+			}
+		}
+	}
+	
 	private Long getNextId() {
 		Long id = 0L;
 		for (Long tmp : ids) {
