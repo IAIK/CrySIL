d20418aea3a14a220b9b949e78a16bfece91b4f6
Otto Touzil <otto.touzil@gmail.com>
2014-02-12 18:24:21 +0100

    objectmanager schaut fertig aus

--- JAVA/src/objects/ObjectManager.java
+++ JAVA/src/objects/ObjectManager.java
@@ -2,6 +2,8 @@ package objects;
 
 import java.util.ArrayList;
 
+import at.iaik.skytrust.element.skytrustprotocol.payload.crypto.key.SKey;
+
 import pkcs11.PKCS11Error;
 import proxys.CK_ATTRIBUTE;
 
@@ -10,7 +12,8 @@ public class ObjectManager {
 	private ArrayList<PKCS11Object> objects = new ArrayList<>();
 	private ArrayList<Long> ids = new ArrayList<>();
 
-	synchronized public ArrayList<PKCS11Object> getObjects(CK_ATTRIBUTE[] template) throws PKCS11Error {
+	synchronized public ArrayList<PKCS11Object> findObjects(
+			CK_ATTRIBUTE[] template) throws PKCS11Error {
 		PKCS11Object object = ObjectBuilder.createFromTemplate(template);
 		ArrayList<PKCS11Object> result = new ArrayList<>();
 
@@ -22,17 +25,36 @@ public class ObjectManager {
 		return result;
 	}
 
-	synchronized public long createObject(CK_ATTRIBUTE[] template) throws PKCS11Error {
+	synchronized public PKCS11Object getObject(long id) {
+		for (Long tmp : ids) {
+			if (tmp == id) {
+				int index = ids.indexOf(tmp);
+				return objects.get(index);
+			}
+		}
+		return null;
+	}
+
+	synchronized public long createObject(CK_ATTRIBUTE[] template)
+			throws PKCS11Error {
 		Long id = getNextId();
 		PKCS11Object object = ObjectBuilder.createFromTemplate(template);
 		objects.add(object);
 		ids.add(id);
 		return id;
 	}
-	
-	synchronized public void deleteObject(long id){
-		for(Long tmp: ids){
-			if(tmp == id){
+
+	synchronized public long createObject(SKey key) throws PKCS11Error {
+		Long id = getNextId();
+		PKCS11Object object = ObjectBuilder.createFromSkyTrust(key);
+		objects.add(object);
+		ids.add(id);
+		return id;
+	}
+
+	synchronized public void deleteObject(long id) {
+		for (Long tmp : ids) {
+			if (tmp == id) {
 				int index = ids.indexOf(tmp);
 				ids.remove(index);
 				objects.remove(index);
@@ -40,7 +62,7 @@ public class ObjectManager {
 			}
 		}
 	}
-	
+
 	private Long getNextId() {
 		Long id = 0L;
 		for (Long tmp : ids) {
