c3b6ce52c9215915a2f65b6ed06d52d8c500f352
Otto Touzil <otto.touzil@student.tugraz.at>
2014-10-24 14:33:43 +0200

    added missing headerfile

---
+++ C/src/Pkcs11Config.h
@@ -0,0 +1,26 @@
+#ifndef __Pkcs11Config_H
+#define __Pkcs11Config_h
+
+
+#define Tutorial_VERSION_MAJOR @Tutorial_VERSION_MAJOR@
+#define Tutorial_VERSION_MINOR @Tutorial_VERSION_MINOR@
+
+
+
+#ifndef WIN32
+#include "pthread\pthread.h"
+
+
+#else
+#include <pthread.h>
+
+
+#endif
+
+
+
+
+
+
+
+#endif
