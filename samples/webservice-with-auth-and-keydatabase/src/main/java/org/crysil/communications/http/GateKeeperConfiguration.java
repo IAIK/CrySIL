480bca7db286644a5d3246b63971ce54781734e5
Florian Reimair <florian.reimair@iaik.tugraz.at>
2017-08-28 15:39:32 +0200

    first database connection

--- samples/webservice-with-auth-and-keydatabase/src/main/java/org/crysil/communications/http/GateKeeperConfiguration.java
+++ samples/webservice-with-auth-and-keydatabase/src/main/java/org/crysil/communications/http/GateKeeperConfiguration.java
@@ -1,5 +1,10 @@
 package org.crysil.communications.http;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.crysil.errorhandling.AuthenticationFailedException;
 import org.crysil.gatekeeper.AuthPlugin;
 import org.crysil.gatekeeper.AuthProcess;
@@ -12,6 +17,38 @@ public class GateKeeperConfiguration implements Configuration {
 	@Override
 	public AuthProcess getAuthProcess(Request request, Gatekeeper gatekeeper) throws AuthenticationFailedException {
 		// create database connection
+		try {
+			// create our mysql database connection
+			String myDriver = "com.mysql.jdbc.Driver";
+			String myUrl = "jdbc:mysql://localhost/cloudks_dev";
+			Class.forName(myDriver);
+			Connection conn = DriverManager.getConnection(myUrl, "cloudks", "cloudkspassword");
+
+			// our SQL SELECT query.
+			// if you only need a few columns, specify them by name instead of
+			// using "*"
+			String query = "SELECT * FROM keyslots";
+
+			// create the java statement
+			Statement st = conn.createStatement();
+
+			// execute the query, and get a java resultset
+			ResultSet rs = st.executeQuery(query);
+
+			// iterate through the java resultset
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String firstName = rs.getString("name");
+				String lastName = rs.getString("description");
+
+				// print the results
+				System.out.format("%s, %s, %s\n", id, firstName, lastName);
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println("Got an exception! ");
+			System.err.println(e.getMessage());
+		}
 		// find appropriate auth information
 		// assemble plugins
 		// order plugins
