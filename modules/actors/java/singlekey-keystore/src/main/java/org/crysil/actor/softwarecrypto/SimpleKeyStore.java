5f5fe0e412059e33e65c8b65416f3c75cd395e49
Florian Reimair <florian.reimair@iaik.tugraz.at>
2017-11-22 12:21:51 +0100

    renew certificate of simplekeyservice

--- modules/actors/java/singlekey-keystore/src/main/java/org/crysil/actor/softwarecrypto/SimpleKeyStore.java
+++ modules/actors/java/singlekey-keystore/src/main/java/org/crysil/actor/softwarecrypto/SimpleKeyStore.java
@@ -1,17 +1,37 @@
 package org.crysil.actor.softwarecrypto;
 
 import java.io.ByteArrayInputStream;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.SignatureException;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.security.auth.x500.X500Principal;
+
+import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
+import org.bouncycastle.asn1.x509.KeyPurposeId;
+import org.bouncycastle.asn1.x509.X509Extensions;
+import org.bouncycastle.asn1.x509.X509Name;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
 import org.crysil.errorhandling.InvalidCertificateException;
 import org.crysil.errorhandling.KeyNotFoundException;
 import org.crysil.errorhandling.KeyStoreUnavailableException;
@@ -27,9 +47,10 @@ import com.google.common.io.BaseEncoding;
 public class SimpleKeyStore implements SoftwareCryptoKeyStore {
 
 	/** the raw key data */
-	private static final String rawPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwQSeSuzJ9MndkJeBkymovvDiFjCOYsd8zPgBGw2+euFfZI3aNJ32dZWMfxW1A2qDF39MazLsCAsE6Z2Vo5BQXz2xEiPlmTko8cUrjM9Qsj0WXc0vUmYKbPsrghco9tQ98PMVQn95kUUTpLDOSPFTebL6rolU+WEtdC1LQtrLaNhrowWJ0OnxtGJINc//ZZFNroHWzdmg6rlu+BojePSBzEoR7/2UmeGQK47vE7KsOl4zMW99E6njCp7vanDnK92XlOfHkAH8pYpnBDzbBBwdKgTok6sfP18/HoQGN853qSq2PXLgk6CNrJoKHB5N9Lp1REO6yCZMarm4eRYCd2GIhwIDAQAB";
-	private static final String rawPrivateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDBBJ5K7Mn0yd2Ql4GTKai+8OIWMI5ix3zM+AEbDb564V9kjdo0nfZ1lYx/FbUDaoMXf0xrMuwICwTpnZWjkFBfPbESI+WZOSjxxSuMz1CyPRZdzS9SZgps+yuCFyj21D3w8xVCf3mRRROksM5I8VN5svquiVT5YS10LUtC2sto2GujBYnQ6fG0Ykg1z/9lkU2ugdbN2aDquW74GiN49IHMShHv/ZSZ4ZArju8Tsqw6XjMxb30TqeMKnu9qcOcr3ZeU58eQAfylimcEPNsEHB0qBOiTqx8/Xz8ehAY3znepKrY9cuCToI2smgocHk30unVEQ7rIJkxqubh5FgJ3YYiHAgMBAAECggEAA4WwRFzAiLSpeJ0TKXX3ObQzv2l8IxzkGQ5KYJbK1PtbuaKqeFhV4POR5PymxuX3nJVfUWkvsL4rTkfEGu3tUYnlBGFemd1pF4ITO9+AVYzlZYTy8zDpW++dJJE66aNQ3ufR0rYvg4Og08fBU+y3dAhKTO/HDR2p7gkEIaUdeY+AxBdtYB9xEnZ7KNs9Tvcd8gYm7szcq5czNZ5g4kFZ1CM1RjUscG6KwfeTqpmmSDjjQy6PIHra5FCB5WzsuYqKxztXUUE5kdc3334evRJ7GG8SHvXHDllaX/MoDhCIqj9BCuaHyXv7H52KWUcl6gNB2MTuk5ssnHQEVymv9yELmQKBgQDtpGDabkuRdStXLMIpi0V2fJwo61+9tDrj+PNfKaQt/kVPN1L9Aj+3eRx9EBG0dWd8QGXgTiWca9iNKAfDFlbYp+YdUj+CdFBQjx4gaMmQPjS9shp+h8vyKbpe7gNnX5h2KKdt+Ioomeoz9AsU7wr/ddtP1/8bB0UhTBwQrvdnKQKBgQDP7cCclUhrfX6cKjlsH+WZsvRKV4TXa02U1hwQNhDPZWWQpkDMy00RooziPDnND7XrQ6RAD2Ls4K5VXnm+iQ+4sKWQHuknE8cnM2PSGMp25CnFMjnakAJVl2POUnV7fIT+OY0tZnaThMLF8TM20Q0cWrHjcgyC4ykSWAL75fPYLwKBgQDoaOCH/2JMaYjvgsiJFLnkfU3D/x3tS7xkhG6P3QvCJ3DlXjf9VRu3dezUqsiF8mQ48kowKn1CE37/3expcQmSbfHxLyUJknORtcZC7/hg51VxSCP9JxXgScsJWEFf8fALbwr/1BhaSNzx3nSQDpB08nCAD8BgUKXdQLAZ6OPwsQKBgGXt2BEqgUDoWSu260Vc8ZICDw1uj9mGaZa/yywLRPxWaY6aYYPDWbl+ZO/2tCMZQ4XcN+WLZWRX1D5XPPkxeXqBZfgbnxIf+O33nER/EKltuihIMeI53FsXBr863wq1BQEXN2T9KL2yREUCs6d4naO7th6YZxe2wgiTCotvs7TTAoGAJMzf8wYAJPhKGDd08KgXxDTtgntl7kegwnXJFNambg010X83i9yDKvkPZF4Nvy3FT2Bwb/qvWwN8vC9Jb55NotG0JbNVTdf75CS12G/U9YZxAHXAvdDdAUKI1jlL6n0lCU6vcrcCK35mde7lbnicq7TG8F/zEnIt82f7NzKXBDw=";
-	private static final String rawCert = "MIICzjCCAbagAwIBAgIGAVLViUrLMA0GCSqGSIb3DQEBCwUAMBExDzANBgNVBAMTBmNyeXNpbDAeFw0xNjAyMTExMjUxMzBaFw0xNjAxMjcyMTA5NDFaMCMxITAfBgNVBAMMGHN0YXRpY0tleUVuY3J5cHRpb25BY3RvcjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMEEnkrsyfTJ3ZCXgZMpqL7w4hYwjmLHfMz4ARsNvnrhX2SN2jSd9nWVjH8VtQNqgxd/TGsy7AgLBOmdlaOQUF89sRIj5Zk5KPHFK4zPULI9Fl3NL1JmCmz7K4IXKPbUPfDzFUJ/eZFFE6SwzkjxU3my+q6JVPlhLXQtS0Lay2jYa6MFidDp8bRiSDXP/2WRTa6B1s3ZoOq5bvgaI3j0gcxKEe/9lJnhkCuO7xOyrDpeMzFvfROp4wqe72pw5yvdl5Tnx5AB/KWKZwQ82wQcHSoE6JOrHz9fPx6EBjfOd6kqtj1y4JOgjayaChweTfS6dURDusgmTGq5uHkWAndhiIcCAwEAAaMaMBgwFgYDVR0lAQH/BAwwCgYIKwYBBQUHAwgwDQYJKoZIhvcNAQELBQADggEBAAh7motLy9RdpvFCEgqMidrgON+n3570OTBjgePsWxHLXzdWRiKevmAI1VAi7K+Qr7KqdZhE7CM5KM5tmhUJ+9SorPmEPbyeaA8SVMDF0whibena3KorBTqIlTkYLwZL9UXkTnOb876VlijxqABKt/rOTP7dZrgErqgcbbTo8KVi2BueiXjLwlV8CJK4s2BWYcLPdMO+Z0jGIjcI4/wuk+60oR8tb5vUwWH62pXw+1IgpnVrklkkM3tNQ0v38A9xKgrK3c1UL7F9KWpZgsCkUR8lfDP0wHAx+Yd5fDp4vTdxSyH/WydLxy2syo1hyoRSE4SXWJBj+N0C+IgGOX3GsNQ=";
+	private static final String rawPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkOWz5tbm7+q1Ap+y9nv864F7vqtKNED5/Gt3FTUfjwcSL7hKtZBRKjLKNyZDlaeJruojHU6sfZA6W1oQN5DOAU+SdfEeattYIwvtIHsBNeDSN2kxUph+NmQ6hW57Iyd3qeiq+obNTDZXgtfVSc7tFEYWd3hmFx3GlDJN1tvlBAXiXDxKKQuCnbh+PKiDW8yrrzaCnaC0bJl8qfos0FSE7DgHC/tesfTMtPtLyq+tYed4xXocAQWnk04B6KE2XiYKme2Ar6ZdxPq86jj6Jndc5j8LLbf8LrI0Zk3Xlu6ZDH//uqNwSk+0fBv+3DGg8/dHYBihhUfVGTyTjqF9Rr0KeQIDAQAB";
+	private static final String rawPrivateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCQ5bPm1ubv6rUCn7L2e/zrgXu+q0o0QPn8a3cVNR+PBxIvuEq1kFEqMso3JkOVp4mu6iMdTqx9kDpbWhA3kM4BT5J18R5q21gjC+0gewE14NI3aTFSmH42ZDqFbnsjJ3ep6Kr6hs1MNleC19VJzu0URhZ3eGYXHcaUMk3W2+UEBeJcPEopC4KduH48qINbzKuvNoKdoLRsmXyp+izQVITsOAcL+16x9My0+0vKr61h53jFehwBBaeTTgHooTZeJgqZ7YCvpl3E+rzqOPomd1zmPwstt/wusjRmTdeW7pkMf/+6o3BKT7R8G/7cMaDz90dgGKGFR9UZPJOOoX1GvQp5AgMBAAECggEAD8Xjgi3TZJtIAk28Y2rOltvo6rH2gU7v75eKtmUqpu0SAJ4XCuH3heiSvPNmQMakvSTZdvWjHwWJ5rscgBH8LOYat0pMwXzYYLNKCEK/mFv9gkDqGLfHKHhbav5CewqPMxCj+SLeuwzA1LpWSmMzSWVSSYXSljJcdTIvKUQVb20jEt//BQO0lFGeHp1p7eCm3Z41LJO7vTaOa3RuZRdNmm+ovsJ/0G0ET9/YYDSzL8NdBqbbcssnlwkUlGpa8dmvTvja4s2Ut53vkN2L+44DteHQkqGozB55842BVDDY2zwRbv3qRdP2TwQXbX6OIb8mofGudGcYKDsXKxFm3AyI0QKBgQDKB5vaLawjQrlNup/0k1s01Z2cdglfelcVxeC5JFA1el88Kx06C2DG3f+kVkZ7UJkaWEkLN6bDUPaJvpKbEYq8lTLXe4IGCpAPjpEfrTAFMS+qw0/aP9j3ptVKzYnvn9Nzr/vsJe/pKnqvvVbKUC/z2dWfmD/vSJh97/9cLEwaEQKBgQC3muzY0PaUSavbHATVTWGN8xOf9cdXkVlQQJvgjUC1mPW2fqmrWRRhPWmQ/6IUcTsbViv/dCpLZ61d43Lrp17JECgiX5N6kZ2hVKLnBBh0jp+AVhaSMdV9SLXW4BeoZ0NDIMA0KWO2z+CsS11qUWWlTzIWYZRz4rDMyH5rIitB6QKBgHMVKn56Ddh4Jb5VVNCpMAdEt+dshaJRulG4ym4sdVRBRQatOJTb7mDaDMm8K6ILI2uSZulw5hnUgNuuJy0WLQWHp9J/lG2EjtONzrVQnk4W2qRXZaFZRWqtKkrWEoDhdj4sPUEjyyny/LgnDs1oKCisKbgjrifMHrF+iOQkh6VBAoGBAJ4tpbEhTd3xHlLVb8CIOux9J0IfD478lNKT5vbtGnuY/M8tjmUliG5B50nZQjyUVJS3h447+RgfiPejNWWaOwnDlqAOcVMeUowkC3g1ShBYs0dwY1+t+TxHQQmO5DLcS9FwTMZjhhmAPBKTlzY6S/TAJumrRacU/LZgYLFRrazJAoGADDTb5rz8adzXao4okOOMGW5JWgSRt3osOMpmMCy3AfWNO59fJFoVFUVjsVjeknhxleUk8iceZ2xcSMOzWH1u3zxW0Ap0jChuvzyONWW/Ba1Td9Y7/t+pCHazve0tJuf6riLKwHelDD8vTLTDU5tm5Udw7VYHm+dBPseFc7Dv6x4=";
+	private static final String rawCert = "MIICzjCCAbagAwIBAgIGAV/ed2FxMA0GCSqGSIb3DQEBCwUAMBExDzANBgNVBAMTBmNyeXNpbDAeFw0xNzExMjAxMjA0NDZaFw0yMTExMjAxMjA0NDZaMCMxITAfBgNVBAMMGHN0YXRpY0tleUVuY3J5cHRpb25BY3RvcjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAJDls+bW5u/qtQKfsvZ7/OuBe76rSjRA+fxrdxU1H48HEi+4SrWQUSoyyjcmQ5Wnia7qIx1OrH2QOltaEDeQzgFPknXxHmrbWCML7SB7ATXg0jdpMVKYfjZkOoVueyMnd6noqvqGzUw2V4LX1UnO7RRGFnd4ZhcdxpQyTdbb5QQF4lw8SikLgp24fjyog1vMq682gp2gtGyZfKn6LNBUhOw4Bwv7XrH0zLT7S8qvrWHneMV6HAEFp5NOAeihNl4mCpntgK+mXcT6vOo4+iZ3XOY/Cy23/C6yNGZN15bumQx//7qjcEpPtHwb/twxoPP3R2AYoYVH1Rk8k46hfUa9CnkCAwEAAaMaMBgwFgYDVR0lAQH/BAwwCgYIKwYBBQUHAwgwDQYJKoZIhvcNAQELBQADggEBAHDgGSV1mK/4wgUnx6BxSDoIavMtlTY/OptR1Jk/G8R58bfxCSD2guJZ5smUbLHslpVqfOMkYumX5UxkhD+w+L8SyM/jA0qQ561kGX7skC8as2/NRf9jH1GXpUemDSwihVFO1gKvBR5u9Nhh8ojDw9kdGXUAh5ciRTTlAfogvVFZzhvjUiDWHvQD6R+M6HTYnsSZB5ryQfKohcNjBKOVhNxlST81F1XYT+3uiFJj5rqCoaCiuaWMkKffOJsj9D32E+VY3EMAouvkDIXIXSIvmktFf+P/UudU0dhsRdJVnW28PZXvTHrf48gtR+hg1i183ht+dEBXkHJDUm1yM4s1GJg=";
+
 	private static final String rawPublicKeyECDSA = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEsrzMLb8Dr6fz3dWmgvOdK3dZGkVAyGAks5rvbPmjaNK1tTJlYHsv8sQgSLauOSos/R7qLNY2NcLlBH3LqDg99g==";
 	private static final String rawPrivateKeyECDSA = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQg9JrLQtrn6RwH+7V34AiW8jNtuF9lgUluLYplxEf4qHuhRANCAASyvMwtvwOvp/Pd1aaC850rd1kaRUDIYCSzmu9s+aNo0rW1MmVgey/yxCBItq45Kiz9Huos1jY1wuUEfcuoOD32";
 	private static final String rawCertEC = "MIIBlzCCAT2gAwIBAgIJAOTuZwR5nph7MAoGCCqGSM49BAMCMCgxJjAkBgNVBAMMHXN0YXRpY0tleUVuY3J5cHRpb25BY3RvckVDRFNBMB4XDTE2MDcyNjA5MTkwMFoXDTM2MDcyMTA5MTkwMFowKDEmMCQGA1UEAwwdc3RhdGljS2V5RW5jcnlwdGlvbkFjdG9yRUNEU0EwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASyvMwtvwOvp/Pd1aaC850rd1kaRUDIYCSzmu9s+aNo0rW1MmVgey/yxCBItq45Kiz9Huos1jY1wuUEfcuoOD32o1AwTjAdBgNVHQ4EFgQUS0OmriRQ1Pp1ZWyB2i6HBUQKOrswHwYDVR0jBBgwFoAUS0OmriRQ1Pp1ZWyB2i6HBUQKOrswDAYDVR0TBAUwAwEB/zAKBggqhkjOPQQDAgNIADBFAiEApzYobPBo+EKteXBaiKYR+XOtbGIGQBX3+Q0NduXavEgCIB12fofx1gNfpHI7r3NwdK7+3RwnKw2tSkHHTGpgHijB";
@@ -159,46 +180,48 @@ public class SimpleKeyStore implements SoftwareCryptoKeyStore {
 		return result;
 	}
 
-  // /**
-	// * helper for creating the raw key data
-	// */
-	// private void createKeyAndCertificate() throws
-	// CertificateEncodingException, InvalidKeyException,
-	// IllegalStateException, NoSuchProviderException, NoSuchAlgorithmException,
-	// SignatureException {
-	// Security.addProvider(new BouncyCastleProvider());
-	// // generate a key pair
-	// KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA",
-	// "BC");
-	// keyPairGenerator.initialize(2048, new SecureRandom());
-	// KeyPair keyPair = keyPairGenerator.generateKeyPair();
-	//
-	// System.out.println(Base64.toBase64String(keyPair.getPublic().getEncoded()));
-	// System.out.println(Base64.toBase64String(keyPair.getPrivate().getEncoded()));
-	//
-	// // build a certificate generator
-	// X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
-	// X500Principal dnName = new X500Principal("cn=crysil");
-	//
-	// // add some options
-	// certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
-	// certGen.setSubjectDN(new X509Name("cn=staticKeyEncryptionActor"));
-	// certGen.setIssuerDN(dnName); // use the same
-	// // yesterday
-	// certGen.setNotBefore(new Date(System.currentTimeMillis() - 24 * 60 * 60 *
-	// 1000));
-	// // in 2 years
-	// certGen.setNotAfter(new Date(System.currentTimeMillis() + 2 * 365 * 24 *
-	// 60 * 60 * 1000));
-	// certGen.setPublicKey(keyPair.getPublic());
-	// certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
-	// certGen.addExtension(X509Extensions.ExtendedKeyUsage, true,
-	// new ExtendedKeyUsage(KeyPurposeId.id_kp_timeStamping));
-	//
-	// // finally, sign the certificate with the private key of the same
-	// // KeyPair
-	// java.security.cert.X509Certificate cert =
-	// certGen.generate(keyPair.getPrivate(), "BC");
-	// System.out.println(Base64.toBase64String(cert.getEncoded()));
-	// }
+	/**
+	 * helper for creating the raw key data
+	 * @throws KeyStoreUnavailableException 
+	 */
+	public static void main(String[] args) throws CertificateEncodingException, InvalidKeyException,
+			IllegalStateException, NoSuchProviderException, NoSuchAlgorithmException, SignatureException,
+			KeyStoreUnavailableException {
+
+		// uncomment next line to test encodings
+		// new SimpleKeyStore();
+		Security.addProvider(new BouncyCastleProvider());
+		// generate a key pair
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+		keyPairGenerator.initialize(2048, new SecureRandom());
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+		System.out.println("private static final String rawPublicKey = \""
+				+ Base64.toBase64String(keyPair.getPublic().getEncoded()) + "\";");
+		System.out.println("private static final String rawPrivateKey = \""
+				+ Base64.toBase64String(keyPair.getPrivate().getEncoded()) + "\";");
+
+		// build a certificate generator
+		X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
+		X500Principal dnName = new X500Principal("cn=crysil");
+
+		// add some options
+		certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
+		certGen.setSubjectDN(new X509Name("cn=staticKeyEncryptionActor"));
+		certGen.setIssuerDN(dnName); // use the same
+		// yesterday
+		certGen.setNotBefore(new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000));
+		// in 2 years
+		certGen.setNotAfter(new Date(System.currentTimeMillis() + 4L * 365 * 24 * 60 * 60 * 1000));
+		certGen.setPublicKey(keyPair.getPublic());
+		certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+		certGen.addExtension(X509Extensions.ExtendedKeyUsage, true,
+				new ExtendedKeyUsage(KeyPurposeId.id_kp_timeStamping));
+
+		// finally, sign the certificate with the private key of the same
+		// KeyPair
+		java.security.cert.X509Certificate cert = certGen.generate(keyPair.getPrivate(), "BC");
+		System.out
+				.println("private static final String rawCert = \"" + Base64.toBase64String(cert.getEncoded()) + "\";");
+	}
 }
