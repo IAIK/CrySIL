4f5860df37ee2eac027c055e6951b2e7cfb66722
Florian Reimair <florian.reimair@iaik.tugraz.at>
2017-12-06 12:12:31 +0100

    create demo service web service with wizard

--- samples/crysil-server-android/app/src/main/java/org/crysil/WizardActivity.java
+++ samples/crysil-server-android/app/src/main/java/org/crysil/WizardActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import org.crysil.database.DatabaseHandler;
+import org.crysil.database.webservice.WebserviceEntry;
+
 /**
  * Creates a certificate to use for mail signatures which is signed by a (self-signed) CA certificate
  */
@@ -14,6 +17,9 @@ public class WizardActivity extends AbstractActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DatabaseHandler databaseHandler = new DatabaseHandler(WizardActivity.this);
+        databaseHandler.insertWebserviceInfo(new WebserviceEntry("CrySIL Demo Service", "crysil.iaik.tugraz.at", "129.27.142.139", 443, "/tomcat/CrysilAndroidRelayService"));
+        databaseHandler.close();
         setContentView(R.layout.activity_wizard);
     }
 
